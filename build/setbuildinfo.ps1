04349c70d32b8b26d4ac2141ee28c7242a32d651
Brent McCullough <brent@brentm.info>
2011-02-28 21:54:18 -0600

    additional correction to assembly number script

--- build/setbuildinfo.ps1
+++ build/setbuildinfo.ps1
@@ -1,3 +1,3 @@
 param([string]$version, [string]$dir = $(split-path $(gv MyInvocation).Value.InvocationName))
 
-"using System.Reflection; [assembly: AssemblyVersion(`"$version`"), assembly: AssemblyFileVersion(`"$version`")]" | out-file "$dir/../src/common/SolutionInfo.cs"
+"using System.Reflection; [assembly: AssemblyVersion(`"$version`"), AssemblyFileVersion(`"$version`")]" | out-file "$dir/../src/common/SolutionInfo.cs"
