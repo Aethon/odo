5a282c4ac50bc0be9c678c99f3f2c30ca665dc20
Brent McCullough <brent@brentm.info>
2011-03-03 18:27:08 -0600

    reved iSyntaptic.Commons; began layout control

---
+++ src/Odo.Html.UnitTests/QUnit/jspf-layout-tests.js
@@ -0,0 +1,277 @@
+﻿$(function () {
+
+    function _testComparer(x, y) {
+        return x - y;
+    };
+
+    function checkControlLayout(control, layout) {
+        if (layout.width) {
+            ok(control.width() === layout.width, "should have width set to " + layout.width);
+        } else {
+            ok(control.width() === -1, "should have width set to -1 (default)");
+        }
+        if (layout.height) {
+            ok(control.height() === layout.height, "should have width set to " + layout.height);
+        } else {
+            ok(control.height() === -1, "should have height set to -1 (default)");
+        }
+    };
+
+    //------------------------------------------------------------------------
+    module("jspf-layout tests");
+    //------------------------------------------------------------------------
+
+    test("jspf is installed", function () {
+        ok(jspf, "jspf should be defined");
+    });
+
+
+    //------------------------------------------------------------------------
+    module("jspf-layout control tests");
+    //------------------------------------------------------------------------
+
+    test("jspf.createControl with no parameters succeeds", function () {
+        var c = jspf.createControl();
+        ok(c, "should return an object");
+        checkControlLayout(c, {});
+    });
+
+    test("jspf.createControl with empty layout succeeds", function () {
+        var c = jspf.createControl({});
+        ok(c, "should return an object");
+        checkControlLayout(c, {});
+    });
+
+    test("jspf.createControl with layout succeeds", function () {
+        var layout = { width: 10, height: 20 };
+        var c = jspf.createControl(layout);
+
+        ok(c, "should return an object");
+        checkControlLayout(c, layout);
+    });
+
+    test("Control.parent() succeeds and is null", function () {
+        var c = jspf.createControl();
+
+        ok(c.parent() === null, "should be null");
+    });
+
+    test("Control._pvt fails with no key", function () {
+        var c = jspf.createControl();
+
+        raises(function () { c._pvt(); }, "should throw an exception");
+    });
+
+    test("Control._pvt() fails with wrong key", function () {
+        var c = jspf.createControl();
+
+        raises(function () { c._pvt(new Object()); }, "should throw an exception");
+    });
+
+    //------------------------------------------------------------------------
+    module("jspf-layout layOutControl tests");
+    //------------------------------------------------------------------------
+
+    test("jspf.layOutControl with no parameters fails", function () {
+        raises(function () { jspf.layOutControl(); }, "should throw an exception");
+    });
+
+    test("jspf.layOutControl with invalid control fails", function () {
+        raises(function () { jspf.layOutControl({}); }, "should throw an exception");
+    });
+
+    test("jspf.layOutControl succeeds", function () {
+        var layout = { width: 10, height: 20 };
+        var c = jspf.createControl(layout);
+        jspf.layOutControl(c);
+        equals(c.measuredWidth(), layout.width);
+        equals(c.measuredHeight(), layout.height);
+        equals(c.arrangedWidth(), layout.width);
+        equals(c.arrangedHeight(), layout.height);
+    });
+
+    //------------------------------------------------------------------------
+    module("jspf-layout container tests");
+    //------------------------------------------------------------------------
+
+    test("jspf.createContainer with no parameters succeeds", function () {
+        var c = jspf.createContainer();
+        ok(c, "should return an object");
+        checkControlLayout(c, {});
+    });
+
+    test("jspf.createContainer with empty layout succeeds", function () {
+        var c = jspf.createContainer({});
+        ok(c, "should return an object");
+        checkControlLayout(c, {});
+    });
+
+    test("jspf.createContainer with layout succeeds", function () {
+        var layout = { width: 10, height: 20 };
+        var c = jspf.createContainer(layout);
+
+        ok(c, "should return an object");
+        checkControlLayout(c, layout);
+    });
+
+    test("Container.children succeeds and is empty", function () {
+        var c = jspf.createContainer({}).children();
+        equals(c.length, 0);
+    });
+
+    test("Container.addChild with non jspf object fails", function () {
+        var c = jspf.createContainer({});
+        raises(function () { c.addChild({}); }, "should throw an exception");
+    });
+
+    test("Container.addChild with jspf control succeeds", function () {
+        var c = jspf.createContainer({});
+        var ch = jspf.createControl();
+        c.addChild(ch);
+        equals(c.children().length, 1, "should have one child");
+        equals(c.children()[0], ch, "should contain the original child");
+        equals(ch.parent(), c, "should have parent set to the container");
+    });
+
+
+    //------------------------------------------------------------------------
+    module("jspf-layout GridLayout tests");
+    //------------------------------------------------------------------------
+
+    test("jspf.createGridLayout with no parameters succeeds", function () {
+        var c = jspf.createGridLayout();
+        ok(c, "should return an object");
+        checkControlLayout(c, {});
+    });
+
+    test("jspf.createGridLayout with empty layout succeeds", function () {
+        var c = jspf.createGridLayout({});
+        ok(c, "should return an object");
+        checkControlLayout(c, {});
+    });
+
+    test("jspf.createGridLayout with layout succeeds", function () {
+        var layout = { width: 10, height: 20 };
+        var c = jspf.createGridLayout(layout);
+
+        ok(c, "should return an object");
+        checkControlLayout(c, layout);
+    });
+
+
+    //------------------------------------------------------------------------
+    module("jspf-layout StackLayout tests");
+    //------------------------------------------------------------------------
+
+    test("jspf.createStackLayout with no parameters succeeds", function () {
+        var c = jspf.createStackLayout();
+        ok(c, "should return an object");
+        checkControlLayout(c, {});
+    });
+
+    test("jspf.createStackLayout with empty layout succeeds", function () {
+        var c = jspf.createStackLayout({});
+        ok(c, "should return an object");
+        checkControlLayout(c, {});
+    });
+
+    test("jspf.createStackLayout with layout succeeds", function () {
+        var layout = { width: 10, height: 20 };
+        var c = jspf.createStackLayout(layout);
+
+        ok(c, "should return an object");
+        checkControlLayout(c, layout);
+    });
+
+    /*
+    test("mergepass handles empty arrays", function () {
+    var calls = 0;
+    var fn = function () { calls++; };
+    odo.mergepass([], [], _testComparer, { match: fn, leftOnly: fn, rightOnly: fn });
+    equal(0, calls, "fn should never be called for empty arrays");
+    });
+
+    var goodCall = function () { ok(true); };
+    var badCall = function () { ok(false, "should not have made this call"); };
+
+    test("mergepass calls fn for left only", function () {
+    expect(1);
+    odo.mergepass([1], [], _testComparer, { match: badCall, rightOnly: badCall, leftOnly: goodCall });
+    });
+
+    test("mergepass calls fn for right only", function () {
+    expect(1);
+    odo.mergepass([], [1], _testComparer, { match: badCall, rightOnly: goodCall, leftOnly: badCall });
+    });
+
+    test("mergepass calls fn for left/right match", function () {
+    expect(1);
+    odo.mergepass([1], [1], _testComparer, { match: goodCall, rightOnly: badCall, leftOnly: badCall });
+    });
+
+    test("mergepass calls fn for both unmatched", function () {
+    expect(2);
+    odo.mergepass([1], [2], _testComparer, { match: badCall, rightOnly: goodCall, leftOnly: goodCall });
+    });
+
+    test("mergepass adds left before", function () {
+    var left = [1];
+    odo.mergepass(left, [0], _testComparer, { match: badCall, rightOnly: function (l, r, li, ri, mod) { mod.addLeft(r); }, leftOnly: goodCall });
+    ok(left.length == 2 && left[0] == 0 && left[1] == 1);
+    });
+
+    test("mergepass adds left after", function () {
+    var left = [1];
+    odo.mergepass(left, [2], _testComparer, { match: badCall, rightOnly: function (l, r, li, ri, mod) { mod.addLeft(r); } });
+    ok(left.length == 2 && left[0] == 1 && left[1] == 2);
+    });
+
+    test("mergepass removes first left", function () {
+    var left = [1, 2, 3];
+    odo.mergepass(left, [1], _testComparer, { match: function (l, r, li, ri, mod) { mod.removeLeft(); }, rightOnly: badCall });
+    ok(left.length == 2 && left[0] == 2 && left[1] == 3);
+    });
+
+    test("mergepass removes mid left", function () {
+    var left = [1, 2, 3];
+    odo.mergepass(left, [2], _testComparer, { match: function (l, r, li, ri, mod) { mod.removeLeft(); }, rightOnly: badCall });
+    ok(left.length == 2 && left[0] == 1 && left[1] == 3);
+    });
+
+    test("mergepass removes last left", function () {
+    var left = [1, 2, 3];
+    odo.mergepass(left, [3], _testComparer, { match: function (l, r, li, ri, mod) { mod.removeLeft(); }, rightOnly: badCall });
+    ok(left.length == 2 && left[0] == 1 && left[1] == 2);
+    });
+
+    test("mergepass adds right before", function () {
+    var right = [1];
+    odo.mergepass([0], right, _testComparer, { match: badCall, leftOnly: function (l, r, li, ri, mod) { mod.addRight(l); }, rightOnly: goodCall });
+    ok(right.length == 2 && right[0] == 0 && right[1] == 1);
+    });
+
+    test("mergepass adds right after", function () {
+    var right = [1];
+    odo.mergepass([2], right, _testComparer, { match: badCall, leftOnly: function (l, r, li, ri, mod) { mod.addRight(l); } });
+    ok(right.length == 2 && right[0] == 1 && right[1] == 2);
+    });
+
+    test("mergepass removes first right", function () {
+    var right = [1, 2, 3];
+    odo.mergepass([1], right, _testComparer, { match: function (l, r, li, ri, mod) { mod.removeRight(); }, leftOnly: badCall });
+    ok(right.length == 2 && right[0] == 2 && right[1] == 3);
+    });
+
+    test("mergepass removes mid right", function () {
+    var right = [1, 2, 3];
+    odo.mergepass([2], right, _testComparer, { match: function (l, r, li, ri, mod) { mod.removeRight(); }, leftOnly: badCall });
+    ok(right.length == 2 && right[0] == 1 && right[1] == 3);
+    });
+
+    test("mergepass removes last right", function () {
+    var right = [1, 2, 3];
+    odo.mergepass([3], right, _testComparer, { match: function (l, r, li, ri, mod) { mod.removeRight(); }, leftOnly: badCall });
+    ok(right.length == 2 && right[0] == 1 && right[1] == 2);
+    });
+    */
+});  
---
+++ src/Odo.Html/Scripts/Source/jspf-layout.js
@@ -0,0 +1,197 @@
+﻿// dependencies:
+//      jQuery
+
+var jspf = jspf || {};
+
+(function($) {
+
+    var defaultLayout = {
+        width: -1,
+        height: -1 //,
+        //margin: [ 0, 0, 0, 0 ],
+        //padding: [ 0, 0, 0, 0],
+        //halign: 'left' | 'right' | 'center' | 'stretch',
+        //valign: 'top' | 'bottom' | 'middle' | 'stretch'
+    };
+
+    var defaultMeasurement = {
+        width: 0,
+        height: 0
+    };
+
+    var defaultArrangement = {
+        width: 0,
+        height: 0,
+        top: 0,
+        left: 0
+    };
+    
+    var _defaultConstraints = {
+        maxWidth: -1,
+        maxHeight: -1
+    };
+
+    // creates a new object based on the given object
+    jspf.create = function(proto) {
+        var F = function() {};
+        F.prototype = proto;
+        return new F();
+    };
+
+    var Control = function() {};
+    var PrivateControlInterface = function() {};
+
+    // this must be passed to Control._pvt to get the private interface
+    var _privateKey = new Object();
+
+    // recursively lays out a top level control
+    jspf.layOutControl = function(control, constraints) {
+        if (!jspf.is(control, "Control"))
+            throw "control is not valid for layout";
+        var c = $.extend($.extend({}, _defaultConstraints), constraints || {});
+
+        var pvt = control._pvt(_privateKey);
+        pvt.measure(constraints);
+        pvt.arrange({ width: control.measuredWidth(), height: control.measuredHeight(), top: 0, left: 0 });
+    };
+
+    // determines if [object] is a jspf Control of [typename] type
+    jspf.is = function(object, typename) {
+        return (object && object instanceof Control && object.is(typename));
+    };
+
+    // creates a basic jspf Control (no real value, directly; used by derived types)
+    jspf.createControl = function(layout) {
+        var _control = jspf.create(new Control());
+    
+        var _layout = $.extend($.extend({}, defaultLayout), layout || {});
+        var _measurement = $.extend({}, defaultMeasurement);
+        var _arrangement = $.extend({}, defaultArrangement);
+
+        var _parent = null;
+        var _pvt = jspf.create(new PrivateControlInterface());
+
+        _pvt.setParent = function(parent) {
+            _parent = parent;
+        }
+
+        _pvt.measure = function(constraints) {
+            _measurement.width = _layout.width;
+            _measurement.height = _layout.height;
+        };
+
+        _pvt.arrange = function(arrangement) {
+            _arrangement = arrangement;
+        };
+
+        _control.is = function(typename) {
+            return typename === "Control";
+        }
+
+        _control.width = function(width) {
+            if (arguments.length == 0) {
+                return _layout.width;
+            }
+            _layout.width = width;
+            return _control;
+        };
+
+        _control.height = function(height) {
+            if (arguments.length == 0) {
+                return _layout.height;
+            }
+            _layout.height = height;
+            return _control;
+        };
+
+        _control.measuredWidth = function() {
+            return _measurement.width;
+        };
+
+        _control.measuredHeight = function() {
+            return _measurement.height;
+        };
+
+        _control.arrangedWidth = function() {
+            return _arrangement.width;
+        };
+
+        _control.arrangedHeight = function() {
+            return _arrangement.height;
+        };
+
+        _control.parent = function() {
+            return _parent;
+        };
+
+        _control._pvt = function(pk) {
+            if (!pk || pk !== _privateKey) {
+                throw "must include the private key to acquire the private interface"; 
+            }
+            return _pvt;    
+        };
+
+        return _control;
+    };
+
+    // creates a basic jspf Container (no real value, directly; used by derived types)
+    jspf.createContainer = function(layout) {
+        var _super = jspf.createControl(layout);
+        var _control = jspf.create(_super);
+
+        var _children = [];
+
+        _control.is = function(typename) {
+            if (typename === "Container") {
+                return true;
+            }
+            return _super.is(typename);
+        }
+
+        _control.addChild = function(child) {
+            if (!jspf.is(child, "Control"))
+                throw "must be a jspf Control";
+            _children.push(child);
+            child._pvt(_privateKey).setParent(_control);
+            return _control;
+        };
+
+        _control.children = function() {
+            return $.merge([], _children);
+        }
+
+        return _control;
+    };
+
+    // creates a jspf GridLayout
+    jspf.createGridLayout = function(layout) {
+        var _super = jspf.createContainer(layout);
+        var _control = jspf.create(_super);
+
+        _control.is = function(typename) {
+            if (typename === "GridLayout") {
+                return true;
+            }
+            return _super.is(typename);
+        }
+
+        return _control;
+    };
+
+    // creates a jspf GridStackLayout
+    jspf.createStackLayout = function(layout) {
+        var _super = jspf.createContainer(layout);
+        var _control = jspf.create(_super);
+
+        _control.is = function(typename) {
+            if (typename === "StackLayout") {
+                return true;
+            }
+            return _super.is(typename);
+        }
+
+        return _control;
+    };
+
+})(jQuery);
+
